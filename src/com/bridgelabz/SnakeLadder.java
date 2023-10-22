7d066384eccdbdae60c139e442f66eeb21c0078d
Rak65 <rakeshp6505@gmail.com>
2023-10-22 19:31:11 +0530

    [Rakesh] Add -: Repeat till the Player reaches the winning position 100

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -7,28 +7,40 @@ public class SnakeLadder {
         final int NoPlay = 0;
         final int Ladder = 1;
         final int Snake = 2;
-        int PlayerPosition = 0;
+        int PlayerPosition = 0; // initial position of player
 
-        // dice number generator
-        int die = (int) (Math.random() * 6) + 1;
-        System.out.println("The Dice Number is :" + die);
+        // loop till player reach the winning spot which is 100
+        while (PlayerPosition < 100) {
 
-        // option Number generator
-        int option = (int) Math.floor(Math.random() * 10) % 3;
+            // dice number generator
+            int die = (int) (Math.random() * 6) + 1;
+            System.out.println("The Dice Number is :" + die);
 
-        if (option == 1) {
-            PlayerPosition = PlayerPosition + die;
-            System.out.println("Your status is Ladder");
-            System.out.println("Your Position is :" + PlayerPosition);
-        } else if (option == 2) {
+            // option Number generator
+            int option = (int) Math.floor(Math.random() * 10) % 3;
+            System.out.println("Generate Random Position :" + option);
 
-            PlayerPosition = PlayerPosition - die;
-            System.out.println(" Your status is Snake");
-            System.out.println("Your Position is :" + PlayerPosition);
+            if (option == 1) {
+                PlayerPosition = PlayerPosition + die;
+                System.out.println("Your status is Ladder");
+                // printing position of player
+                System.out.println("Your Position is :" + PlayerPosition);
 
-        } else {
-            System.out.println("Your Status is No Play :");
-            System.out.println("Your Position is :" + PlayerPosition);
+            }
+            else if (option == 2) {
+                // In case the player position moves below 0, then the player restarts from 0
+                if (PlayerPosition > 0 && (PlayerPosition - die) >= 0) {
+                    PlayerPosition = PlayerPosition - die;
+                    System.out.println(" Your status is Snake");
+                    System.out.println("Your Position is :" + PlayerPosition);
+                }
+            }
+            else {
+                System.out.println("Your Status is No Play ");
+                System.out.println("Your Position is :" + PlayerPosition);
+            }
         }
+        System.out.println("Congratulations! You reached the finish line!");
+        System.out.println("Game over!");
     }
 }
