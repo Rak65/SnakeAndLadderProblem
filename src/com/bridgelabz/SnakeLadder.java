0ee79694d6a88b784a409df34872f30188e38e2b
Rak65 <rakeshp6505@gmail.com>
2023-10-23 10:30:29 +0530

    [Rakesh] Add -: Ensure the Player gets to exact winning position 100.

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -10,7 +10,8 @@ public class SnakeLadder {
         int PlayerPosition = 0; // initial position of player
 
         // loop till player reach the winning spot which is 100
-        while (PlayerPosition < 100) {
+        while (PlayerPosition < 100)
+        {
 
             // dice number generator
             int die = (int) (Math.random() * 6) + 1;
@@ -20,27 +21,32 @@ public class SnakeLadder {
             int option = (int) Math.floor(Math.random() * 10) % 3;
             System.out.println("Generate Random Position :" + option);
 
-            if (option == 1) {
-                PlayerPosition = PlayerPosition + die;
+            if (option == 1)
+            {
+                if (PlayerPosition <= 100 && (PlayerPosition + die <= 100))
+                    PlayerPosition = PlayerPosition + die;
                 System.out.println("Your status is Ladder");
+
                 // printing position of player
                 System.out.println("Your Position is :" + PlayerPosition);
-
             }
-            else if (option == 2) {
+
+            else if (option == 2)
+            {
                 // In case the player position moves below 0, then the player restarts from 0
-                if (PlayerPosition > 0 && (PlayerPosition - die) >= 0) {
+                if (PlayerPosition > 0 && (PlayerPosition - die) >= 0)
+                {
                     PlayerPosition = PlayerPosition - die;
                     System.out.println(" Your status is Snake");
                     System.out.println("Your Position is :" + PlayerPosition);
                 }
             }
-            else {
+
+            else
+            {
                 System.out.println("Your Status is No Play ");
                 System.out.println("Your Position is :" + PlayerPosition);
             }
         }
-        System.out.println("Congratulations! You reached the finish line!");
-        System.out.println("Game over!");
     }
 }
